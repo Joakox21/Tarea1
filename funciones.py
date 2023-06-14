a25b04889516b3cecf4d3b96bdc2cb451dc3db03
Joakox21 <joaquin.varasmoraga@gmail.com>
2023-06-14 09:58:54 -0400

    Numeros primo

--- funciones.py
+++ funciones.py
@@ -1,2 +1,9 @@
-print("hola") 
-print ("como estas")
+def es_primo(numero):
+    if numero < 2:
+        return False  # Los números menores a 2 no son primos
+
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            return False  # Se encontró un divisor, por lo tanto no es primo
+
+    return True  # No se encontraron divisores, es primo
